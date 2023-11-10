2a001743d7a0fa2a71e729a9afa1e6d1d4e5ec4d
Nimmaravikiran <94212790+Nimmaravikiran@users.noreply.github.com>
2023-11-10 23:19:55 +0530

    project has modified

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -27,19 +27,21 @@ def listen():
         print(f"Could not request results from Google Speech Recognition service; {e}")
         return ""
 
-def assistant(query):
+def assistant(query, user):
     if 'time' in query:
         current_time = DateTime.datetime.now().strftime("%H:%M:%S")
         speak(f"The current time is {current_time}")
-    elif 'wikipedia' in query:
+    elif user in query:
         query = query.replace("wikipedia", "")
         result = wikipedia.summary(query, sentences=2)
+        print(result)
         speak("According to Wikipedia, " + result)
     else:
         speak("I'm sorry, I didn't understand that.")
 
 if __name__ == "__main__":
-    speak("Hello! How can I assist you today?")
+    name = input("What is your name?  ")
+    speak(f"Hello {name}! How can I assist you today?")
 
     while True:
         user_input = listen()
@@ -47,4 +49,4 @@ if __name__ == "__main__":
             speak("Goodbye!")
             break
         elif user_input:
-            assistant(user_input)
+            assistant(query=user_input, user=name)
