d28befb9cf4786389ff9a20c88a2ec820bec707b
Nimmaravikiran <94212790+Nimmaravikiran@users.noreply.github.com>
2023-11-10 23:08:53 +0530

    project file has been added

---
+++ Voice_Assistant.py
@@ -0,0 +1,50 @@
+import speech_recognition as sr
+import pyttsx3
+import DateTime
+import wikipedia
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = recognizer.recognize_google(audio, language='en')
+        print(f"User: {query}")
+        return query.lower()
+    except sr.UnknownValueError:
+        print("Sorry, I didn't catch that. Could you please repeat?")
+        return ""
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+        return ""
+
+def assistant(query):
+    if 'time' in query:
+        current_time = DateTime.datetime.now().strftime("%H:%M:%S")
+        speak(f"The current time is {current_time}")
+    elif 'wikipedia' in query:
+        query = query.replace("wikipedia", "")
+        result = wikipedia.summary(query, sentences=2)
+        speak("According to Wikipedia, " + result)
+    else:
+        speak("I'm sorry, I didn't understand that.")
+
+if __name__ == "__main__":
+    speak("Hello! How can I assist you today?")
+
+    while True:
+        user_input = listen()
+        if user_input == "exit":
+            speak("Goodbye!")
+            break
+        elif user_input:
+            assistant(user_input)
